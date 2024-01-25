a1c28bf0c78572c7bebb2dcb3b71cd760e5d0f4d
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2024-01-25 20:46:07 +0100

    Organise project folders

---
+++ Touchdown/Touchdown/Utility/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by Pablo Caraballo Gómez on 25/1/24.
+//
+
+import SwiftUI
+
+struct Constanst {
+
+    // MARK: - Color
+    struct Colors {
+        static let colorBackground = Color("ColorBackground")
+        static let colorGray = Color(UIColor.systemGray4)
+    }
+}
