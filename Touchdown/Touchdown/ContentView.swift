4d51a06bd01372d64ad6023ee14a6be79bac8b58
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2024-01-25 20:29:36 +0100

    Add project

---
+++ Touchdown/Touchdown/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Pablo Caraballo Gómez on 25/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Pablo Caraballo Gómez on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
